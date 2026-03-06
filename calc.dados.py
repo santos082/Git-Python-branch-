320796bcefd8c1a05b2f0e79afe8e9a8ffc26437
GUILHERME DA SILVA SANTOS <90041124804@info.local>
2026-03-06 18:49:02 -0300

    feat: distancia euclidiana e priduto escalar

--- calc.dados.py
+++ calc.dados.py
@@ -1,11 +1,22 @@
-# Função para validar se os vetores possuem o mesmo tamanho
 def validar_vetores(v1, v2):
     if len(v1) != len(v2):
         raise ArithmeticError("Os vetores devem ter o mesmo tamanho!")
     return True
 
 
-# Código principal do sistema
+# Distância Euclidiana
+def euclidiana(v1, v2):
+    validar_vetores(v1, v2)
+    soma_quadrados = sum((a - b) ** 2 for a, b in zip(v1, v2))
+    return soma_quadrados ** 0.5
+
+
+# Produto Escalar
+def produto_escalar(v1, v2):
+    validar_vetores(v1, v2)
+    return sum(a * b for a, b in zip(v1, v2))
+
+
 if __name__ == "__main__":
     obs = [1.2, 2.5, 3.8]
     pred = [1.0, 2.7, 4.0]
